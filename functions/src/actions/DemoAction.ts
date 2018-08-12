9edab2abe5ef33523af5d8994a61d161550a2b87
Jags <jags14385@users.noreply.github.com>
2018-08-12 23:24:21 +1000

    try

--- functions/src/actions/DemoAction.ts
+++ functions/src/actions/DemoAction.ts
@@ -1,4 +1,4 @@
-import { DialogflowApp } from 'actions-on-google';
+import { DialogflowApp } from 'actions-on-google'
 
 export const name = 'input.welcome'
 export function handler(app: DialogflowApp) {
