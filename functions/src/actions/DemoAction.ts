efef188434be7f50984fbe263eb4bc2614b7f518
Jags <jags14385@users.noreply.github.com>
2018-08-12 23:48:37 +1000

    trial Work

--- functions/src/actions/DemoAction.ts
+++
@@ -1,6 +0,0 @@
-import { DialogflowApp, Dialogflow } from 'actions-on-google'
-
-export const name = 'input.welcome'
-export function handler(app: Dialogflow) {
-	app.tell("Hi . This is from Demo Action")
-}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,25 +5,8 @@ import {WebhookClient} from 'dialogflow-fulfillment';
 
 admin.initializeApp(functions.config().firebase);
 // const db = admin.database();
-// https://firebase.google.com/docs/functions/typescript
-// Dialogflow Intent names
-const WELCOME_INTENT = 'input.welcome';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-
-    // console.log('headers: ' + JSON.stringify(request.headers));
-    // console.log('body: ' + JSON.stringify(request.body));
-
-    // const dialogflowApp = dialogflow()
-    // interface Action {
-    //     name: string,
-    //     handler: (app: DialogflowApp) => void
-    // }
-    // const actions: Action[] = [
-    // // Add actions here.
-    // DemoAction
-    // ]
-
 
     const agent = new WebhookClient({ request, response });
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
@@ -36,6 +19,10 @@ export const helloWorld = functions.https.onRequest((request, response) => {
     function fallback () {
       agent.add(`I didn't understand`);
       agent.add(`I'm sorry, can you try again?`);
+    }
+
+    function getStationInfo() {
+        agent.add(`Staion intent called`);
     }
 
     const intentMap = new Map();
