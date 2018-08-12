6cea9c738dbfac94427e815e084afd91b4b9584d
Jags <jags14385@users.noreply.github.com>
2018-08-12 22:45:06 +1000

    Adding logic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,34 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as Dialogflow from 'actions-on-google';
+import { dialogflow } from 'actions-on-google';
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.database();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// https://firebase.google.com/docs/functions/typescript
+
+// Dialogflow Intent names
+const WELCOME_INTENT = 'input.welcome';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+
+    console.log('headers: ' + JSON.stringify(request.headers));
+    console.log('body: ' + JSON.stringify(request.body));
+
+    const app = dialogflow();
+
+    app.intent('Default Welcome Intent', conv => {
+        conv.ask('Hi, how is it going?')
+        conv.ask(`Here's a picture of a cat`)
+    });
+
+    // Intent in Dialogflow called `Goodbye`
+    app.intent('Goodbye', conv => {
+        conv.close('See you later!')
+    })
+  
+    app.intent('Default Fallback Intent', conv => {
+        conv.ask(`I didn't understand. Can you tell me something else?`)
+    })
+
 });
